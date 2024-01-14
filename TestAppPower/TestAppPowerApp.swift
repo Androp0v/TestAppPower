8a7a48c559cfc0361943e973093dd2f6d6e59c07
Raúl Montón Pinillos <raul_m_p@me.com>
2024-01-14 16:06:11 +0100

    Initial Commit

---
+++ TestAppPower/TestAppPowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppPowerApp.swift
+//  TestAppPower
+//
+//  Created by Raúl Montón Pinillos on 14/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppPowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
